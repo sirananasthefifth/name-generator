f1ab2219fb378e7b4b5d67f4ae76d225a437b04a
sirananasthefifth <emil.embretsen@elev.ga.ntig.se>
2022-09-14 09:41:39 +0200

    Added last names and methods

--- NameGenerator/NameGenerator/Program.cs
+++ NameGenerator/NameGenerator/Program.cs
@@ -11,31 +11,51 @@
             bool woman = false;
             
 
-            //Fråga användaren om antalet bokstäver och namn
-            Console.WriteLine("Hur många bokstäver ska namnet ha?");
-            int numberOfLetters = Convert.ToInt32(Console.ReadLine());
+            //Fråga användaren om antalet bokstäver, antalet namn och vilket kön
+            Console.WriteLine("Hur många bokstäver ska förnamnet ha?");
+            int firstNameLetters = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hur många bokstäver ska efternamnet ha?");
+            int lastNameLetters = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Hur många namn vill du generera?");
             int numberOfNames = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Skriv 1 för kvinnliga och 2 för manliga namn:");
+            Console.WriteLine("Skriv 1 för kvinnliga namn och 2 för manliga namn:");
             int gender = Convert.ToInt32(Console.ReadLine());
 
+            
             if (gender == 1)
             {
                 woman = true;
             }
 
-            //Generera namn
+            //kallar på generator metoden//
+            Generate(woman, vowels, consonants, firstNameLetters, numberOfNames, lastNameLetters);
+
+
+        }
+
+        static void Generate(bool woman, char[] vowels, char[] consonants, int firstNameLetters, int numberOfNames, int lastNameLetters)
+        {
+             //Generera namn
             Random rnd1 = new Random();
             Random rnd2 = new Random();
             Random rnd3 = new Random();
+            Random rnd4 = new Random();
+            Random rnd5 = new Random();
+            Random rnd6 = new Random();
             //variabler för random systemet 
             int check;
             int cons;
             int vow;
 
+            if (woman)
+            {
+                firstNameLetters--;
+            }
+
             for (int i2 = 0; i2 < numberOfNames; i2++)
             {
-                for (int i = 0; numberOfLetters > i; i++)
+                //Generera förnamn
+                for (int i = 0; firstNameLetters > i; i++)
                 {
                     check = rnd1.Next(2);
 
@@ -54,13 +74,32 @@
                 {
                     Console.Write("A");
                 }
+                Console.Write(" ");
 
-                Console.WriteLine();
+                //Generera efternamn
+                
+
+                
+               //Generera förnamn
+               for (int i3 = 0; i3 < lastNameLetters; i3++)
+               {
+                  check = rnd4.Next(2); // Randomise consonant or vowel
+
+                  if (check == 0)
+                  {
+                     cons = rnd5.Next(consonants.Length);
+                     Console.Write(consonants[cons]);
+                  }
+                  if (check == 1)
+                  {
+                     vow = rnd6.Next(vowels.Length);
+                     Console.Write(vowels[vow]);
+                  }
+                    
+               }
+
+                    Console.WriteLine();
             }
-            
-        
-        
-        
         }
     }
 }
